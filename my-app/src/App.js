d22bf3ce66f48a9f835f930b0b04c5a54a44a608
chandankoranga02 <chandan.koranga.2025@gmail.com>
2026-01-12 12:20:33 +0530

    final app ready

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import ReactUI from './Components/text.mjs';
 import TextForm from './Components/TextForm.mjs';
 import Navbar from './Components/Navbar.mjs';
 import { useState } from 'react';
+import AboutUS from './Components/AboutUS.mjs'
+import { Routes, Route } from "react-router-dom";
+
 
 
 
@@ -14,7 +16,7 @@ function App() {
   const NormalMode = 'white';
 
   const [btnText, changeText] = useState("Enable Dark Mode");
-  const [currentMode, modeChange] = useState({ color : DarkMode, backgroundColor: NormalMode,});
+  const [currentMode, modeChange] = useState({ color: DarkMode, backgroundColor: NormalMode, });
 
 
   let togglebutton = () => {
@@ -26,7 +28,7 @@ function App() {
       })
       changeText("Enable Light mode");
       document.body.style.backgroundColor = DarkMode
-      
+
     }
 
     else {
@@ -37,7 +39,7 @@ function App() {
       })
       changeText("Enable Dark mode");
       document.body.style.backgroundColor = NormalMode
-      
+
 
     }
   }
@@ -46,9 +48,12 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar inputMode={currentMode}  stylemode={currentMode.backgroundColor === DarkMode ? 'dark' : 'light'} > </Navbar>
+      <Navbar inputMode={currentMode} stylemode={currentMode.backgroundColor === DarkMode ? 'dark' : 'light'} > </Navbar>
       <button type="button" className={`btn btn-${currentMode.backgroundColor === DarkMode ? 'light' : 'dark'} my-5`} onClick={togglebutton} > {btnText} </button>
-      <TextForm stylemode={currentMode} />
+      <Routes>
+        <Route  path="/" element={<TextForm stylemode={currentMode} />}/>
+        <Route  path="/about" element={<AboutUS stylemode={currentMode}  />}/>
+      </Routes>
 
     </div>
 
